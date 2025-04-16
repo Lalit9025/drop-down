2b74fe96be5dbb7e3bb4b18be6c11a3fa44d2b9a
Lalit9025 <lalit3chouhan@gmail.com>
2025-04-16 10:35:24 +0530

    image added in public

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import './Gallery.css'
-import p1 from '../Images/p1.jpg'
+
 
 const Gallery = () => {
   return (
     <div className='gallery'>
         <div className="gallery_combo">
             <div className="gallery_element">
-                <img src={p1} alt='img'/>
+                <img src="/p1.jpg" alt='img'/>
                 <span className="product_name">Product #1</span>
             </div>
             <div className="gallery_element">
-               <img src={p1} alt='img'/>
+               <img src="/p1.jpg" alt='img'/>
                 <span className="product_name">Product #2</span>
             </div>
         </div>
         <div className="gallery_combo">
             <div className="gallery_element">
-                <img src={p1} alt='img'/>
+                <img src="/p1.jpg" alt='img'/>
                 <span className="product_name">Product #3</span>
             </div>
             <div className="gallery_element">
-                <img src={p1} alt='img'/>    
+                <img src="/p1.jpg" alt='img'/>    
                 <span className="product_name">Product #4</span>
             </div>
         </div>
